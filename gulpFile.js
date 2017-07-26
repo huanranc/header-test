daa84df5efdabe64e332d8c70db03f9ffb9d984f
huanran <huanranjoe@163.com>
2017-07-25 18:35:46 -0700

    sixth commit

--- gulpFile.js
+++ gulpFile.js
@@ -1,17 +1,9 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-gulp.task('header',function(){
-	console.log('hello');
-	return gulp.src('header/sass/style.scss')
-	    .on('error',sass.logError)
-		.pipe(sass())
-		.pipe(gulp.dest('header/css'))
-	});
-
 gulp.task('nav',function(){
 	console.log('hello nav');
-	return gulp.src('test/sass/style.scss')
+	return gulp.src('project/sass/style.scss')
 	    .on('error',sass.logError)
 		.pipe(sass())
-		.pipe(gulp.dest('test/css'))
+		.pipe(gulp.dest('project/css'))
 	});
