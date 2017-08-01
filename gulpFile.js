cfb7802059954e889fc69e0b2ade543d1ae5bf62
huanran <huanranjoe@163.com>
2017-08-01 00:39:59 -0700

    new commit update

--- gulpFile.js
+++ gulpFile.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-<<<<<<< HEAD
 var htmlhint = require("gulp-htmlhint");
 var csslint = require('gulp-csslint');
 var gulpStylelint = require('gulp-stylelint');
@@ -43,13 +42,3 @@ gulp.task('lint-css', function lintCssTask() {
       ]
     }));
 });
-=======
-gulp.task('nav',function(){
-	console.log('hello nav');
-	return gulp.src('project/sass/style.scss')
-	    .on('error',sass.logError)
-		.pipe(sass())
-		.pipe(gulp.dest('project/css'))
-	});
-gulp.watch('project/sass/style.scss', ['A']);
->>>>>>> 3dc56b74454e14080709d20c9c761bedf39d8a5a
